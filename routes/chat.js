b9467a9d3451d3ef006cda62b57edcabed96b2c3
Ahn-GiHwan <ahngh0113@naver.com>
2021-10-07 18:18:06 +0900

    feat: Route and Render for /chat

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res, next) => {
-  return res.send("chatRouter works!!!!!!");
+  res.render("chatroom");
 });
 
 module.exports = router;
