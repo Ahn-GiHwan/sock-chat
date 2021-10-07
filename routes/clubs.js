e6efc134ab7ab0e7dc3a76b017dd11ec78ed1365
Ahn-GiHwan <ahngh0113@naver.com>
2021-10-07 17:37:19 +0900

    feat: Add PL Club data

--- routes/clubs.js
+++ routes/clubs.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+const csv = require("../data");
 
 router.route("/").get((req, res, next) => {
   return res.send("clubsRouter works!!!!!!");
 });
 
 router.route("/:filename/").get((req, res, next) => {
-  const filename = req.params.filename;
+  const fileName = req.params.filename;
+  const filePath = path.join(__dirname, "..", "data", fileName + ".csv");
 
-  res.send(filename);
+  const toSend = csv(filePath);
+  res.json(toSend);
 });
 
 module.exports = router;
